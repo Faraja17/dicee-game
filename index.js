1ee19739844e99c5b3325c32c1144d99f78d427c
Faraja17 <farajakthompson@gmail.com>
2022-06-06 12:46:05 -0400

    randomized the dice

---
+++ index.js
@@ -0,0 +1,43 @@
+// Randomize Dice
+
+let randomNumber1 = Math.floor(Math.random() * 6);
+let randomNumber2 = Math.floor(Math.random() * 6);
+
+if (randomNumber1 === 0) {
+    document.querySelector(".img1").setAttribute("src", "images/dice1.png");
+}
+else if (randomNumber1 === 1) {
+    document.querySelector(".img1").setAttribute("src", "images/dice2.png");
+}
+else if (randomNumber1 === 2) {
+    document.querySelector(".img1").setAttribute("src", "images/dice3.png");
+}
+else if (randomNumber1 === 3) {
+    document.querySelector(".img1").setAttribute("src", "images/dice4.png");
+}
+else if (randomNumber1 === 4) {
+    document.querySelector(".img1").setAttribute("src", "images/dice5.png");
+}
+else if (randomNumber1 === 5) {
+    document.querySelector(".img1").setAttribute("src", "images/dice6.png");
+}
+
+if (randomNumber2 === 0) {
+    document.querySelector(".img2").setAttribute("src", "images/dice1.png");
+}
+else if (randomNumber2 === 1) {
+    document.querySelector(".img2").setAttribute("src", "images/dice2.png");
+}
+else if (randomNumber2 === 2) {
+    document.querySelector(".img2").setAttribute("src", "images/dice3.png");
+}
+else if (randomNumber2 === 3) {
+    document.querySelector(".img2").setAttribute("src", "images/dice4.png");
+}
+else if (randomNumber2 === 4) {
+    document.querySelector(".img2").setAttribute("src", "images/dice5.png");
+}
+else if (randomNumber2 === 5) {
+    document.querySelector(".img2").setAttribute("src", "images/dice6.png");
+}
+
