964848a6345407344a79a51b4b6333df342d2f03
Faraja17 <farajakthompson@gmail.com>
2022-06-06 13:22:14 -0400

    corrected +1
    to get random numbers from 1-6 rather than from 0-5.

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
 // Randomize the dice.
 
-let randomNumber1 = Math.floor(Math.random() * 6);
-let randomNumber2 = Math.floor(Math.random() * 6);
+let randomNumber1 = Math.floor(Math.random() * 6 + 1);
+let randomNumber2 = Math.floor(Math.random() * 6 + 1);
 
-if (randomNumber1 === 0) {
+if (randomNumber1 === 1) {
     document.querySelector(".img1").setAttribute("src", "images/dice1.png");
 }
-else if (randomNumber1 === 1) {
+else if (randomNumber1 === 2) {
     document.querySelector(".img1").setAttribute("src", "images/dice2.png");
 }
-else if (randomNumber1 === 2) {
+else if (randomNumber1 === 3) {
     document.querySelector(".img1").setAttribute("src", "images/dice3.png");
 }
-else if (randomNumber1 === 3) {
+else if (randomNumber1 === 4) {
     document.querySelector(".img1").setAttribute("src", "images/dice4.png");
 }
-else if (randomNumber1 === 4) {
+else if (randomNumber1 === 5) {
     document.querySelector(".img1").setAttribute("src", "images/dice5.png");
 }
-else if (randomNumber1 === 5) {
+else if (randomNumber1 === 6) {
     document.querySelector(".img1").setAttribute("src", "images/dice6.png");
 }
 
-if (randomNumber2 === 0) {
+if (randomNumber2 === 1) {
     document.querySelector(".img2").setAttribute("src", "images/dice1.png");
 }
-else if (randomNumber2 === 1) {
+else if (randomNumber2 === 2) {
     document.querySelector(".img2").setAttribute("src", "images/dice2.png");
 }
-else if (randomNumber2 === 2) {
+else if (randomNumber2 === 3) {
     document.querySelector(".img2").setAttribute("src", "images/dice3.png");
 }
-else if (randomNumber2 === 3) {
+else if (randomNumber2 === 4) {
     document.querySelector(".img2").setAttribute("src", "images/dice4.png");
 }
-else if (randomNumber2 === 4) {
+else if (randomNumber2 === 5) {
     document.querySelector(".img2").setAttribute("src", "images/dice5.png");
 }
-else if (randomNumber2 === 5) {
+else if (randomNumber2 === 6) {
     document.querySelector(".img2").setAttribute("src", "images/dice6.png");
 }
 
